360bb8e6cffb35b326ff2c0ba21b1218484f8cb5
jarciniegas20 <jarciniegas68w@gmail.com>
2023-07-14 15:42:04 -0600

    Edited the hooray variable

--- day.js
+++ day.js
@@ -11,7 +11,7 @@
         let daysCalc = 5 - date.getDay(); // Friday minus the current day index. Sunday is 0
         let daysText = daysCalc === 1 ? "day" : "days"; // If it's 1 day until Friday, use "day", else "days"
         let isAre = daysCalc === 1 ? "is" : "are"; // If it's 1 day until Friday, use "is", else "are"
-        let yayOrNay = daysCalc === 1 ? "Hooray for the weekend!" : ""; // If it's 1 day until Friday, say "hooray..."
+        let yayOrNay = (daysCalc > -3) && (daysCalc < 1) ? "Hooray for the weekend!" : ""; // If it's 1 day until Friday, say "hooray..."
         return `Welcome to my page! The current day is ${this.currentDay(date)}.\n` +
                `There ${isAre} ${daysCalc} ${daysText} until Friday. ${yayOrNay}`
   
